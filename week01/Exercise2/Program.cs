e93d4d57a18891dc630707950800863bd316e4e8
luciana-tech <luciana.lds@gmail.com>
2026-01-09 10:33:03 -0300

    Completed w1 exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("What is your grade percentage? ");
+        string answer = Console.ReadLine();
+        int percent = int.Parse(answer);
+
+        string letter = "";
+
+        if (percent >= 90)
+        {
+            letter = "A";
+        } 
+        else if (percent >= 80)
+        {
+            letter = "B";
+        }
+         else if (percent >= 70)
+        {
+            letter = "C";
+        }
+         else if (percent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"Ÿour grade is {letter}");
+
+        if (percent>=70)
+        {
+            Console.WriteLine("Congratulations, you passed!");
+        }  
+        else
+        {
+            Console.WriteLine("Keep trying!");
+        }  
     }
+        
 }
