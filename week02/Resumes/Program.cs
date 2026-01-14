626cdf1fb1ae8bacce5f2568a7d046ea1b934e10
luciana-tech <luciana.lds@gmail.com>
2026-01-13 21:50:36 -0300

    Completed w2 exercise Abstraction

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+     Job job1 = new Job();
+     job1._jobTitle = "Software Developer";
+     job1._company = "Microsoft";
+     job1._startYear = 2019;
+     job1._endYear = 2021;
+
+     Job job2 = new Job();
+     job2._jobTitle = "Software Architect";
+     job2._company = "Apple";
+     job2._startYear = 2022;
+     job2._endYear = 2024;
+     
+     Resume myResume = new Resume();
+     myResume._personName = "Luciana Oliveira";
+     myResume._jobs.Add(job1);
+     myResume._jobs.Add(job2);
+
+     myResume.Display();
+
     }
+    
 }
