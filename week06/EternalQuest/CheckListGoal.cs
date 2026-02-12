923d371bc466447dd541c947d8ba4b151ae88d48
luciana-tech <luciana.lds@gmail.com>
2026-02-12 17:41:49 -0300

    W6 EternalQuest Design

---
+++ week06/EternalQuest/CheckListGoal.cs
@@ -0,0 +1,34 @@
+using System;
+
+public class ChecklistGoal : Goal
+{
+    private int _amountCompleted;
+    private int _target;
+    private int _bonus;
+
+    public ChecklistGoal(string name, string description, int points, int target, int bonus) : base(name, description, points)  
+    {
+        _amountCompleted = 0;
+        _target = target;
+        _bonus = bonus;
+    }
+
+    public override void RecordEvent()
+    {
+    }
+
+    public override bool IsComplete()
+    {
+        return _amountCompleted >= _target;
+    }
+
+    public override string GetDetailsString()
+    {
+        return "";
+    }
+
+    public override string GetStringsRepresentation()
+    {
+        return "";
+    }
+}
---
+++ week06/EternalQuest/SimpleGoal.cs
@@ -0,0 +1,25 @@
+using System;
+
+public class SimpleGoal : Goal
+{
+    private bool _isComplete;
+
+    public SimpleGoal(string name, string description, int points) : base(name, description, points) 
+    {
+        _isComplete = false;
+    }
+
+    public override void RecordEvent()
+    {
+    }
+
+    public override bool IsComplete()
+    {
+        return _isComplete;
+    }
+
+    public override string GetStringsRepresentation()
+    {
+        return "";
+    }
+}
